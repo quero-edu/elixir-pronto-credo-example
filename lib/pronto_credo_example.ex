044c8708946a589e478ebdf4f9f951456fda0b29
Thiago Brandão <thiagobrandam@gmail.com>
2017-09-28 17:02:01 -0300

    Adds boilerplate code
    Minimum setup to make credo work

---
+++ lib/pronto_credo_example.ex
@@ -0,0 +1,18 @@
+defmodule ProntoCredoExample do
+  @moduledoc """
+  Documentation for ProntoCredoExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProntoCredoExample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule ProntoCredoExample.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :pronto_credo_example,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
