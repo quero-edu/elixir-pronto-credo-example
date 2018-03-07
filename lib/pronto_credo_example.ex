b3039a838545de9879a6e57507fae8c692e6e051
Thiago Brandão <thiagobrandam@gmail.com>
2018-03-07 15:46:40 -0300

    Adds another function to trigger credo

--- lib/pronto_credo_example.ex
+++ lib/pronto_credo_example.ex
@@ -23,4 +23,8 @@ defmodule ProntoCredoExample do
   def another_func_with_name_that_is_so_long_that_its_signature_takes_up_more_than_80_spaces(a, b, c, d) do
     1 + 4
   end
+
+  def another_func_with_name_that_is_so_long_that_its_signature_takes_up_more_than_80_spaces(a, b, c, d) do
+    1 + 4
+  end
 end
